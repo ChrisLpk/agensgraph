a474c37482fd113eb45b7e46e8375def1b93cf17
ChrisLpk <48610501+ChrisLpk@users.noreply.github.com>
2019-04-12 00:25:24 +0800

    Update analyze.c

--- src/backend/parser/analyze.c
+++ src/backend/parser/analyze.c
@@ -336,6 +336,16 @@ transformStmt(ParseState *pstate, Node *parseTree)
 			result = transformCreateTableAsStmt(pstate,
 												(CreateTableAsStmt *) parseTree);
 			break;
+			
+		case T_CopyRdfStmt:
+			{
+				CopyRdfStmt   *n = (CopyRdfStmt *) parseTree;
+
+				result = makeNode(Query);
+				result->commandType = CMD_UTILITY;
+				result->utilityStmt = (Node *) parseTree;
+			}
+			break;
 
 		default:
 
